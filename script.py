4ab9bc539ff46694d4647dbd5f179c7d0987d048
Dr. Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-09 14:30:05 -0600

    Fix typos, unnecessary imports

--- script.py
+++ script.py
@@ -3,4 +3,4 @@ from functions import cleaning_funcs as cf
 mypath = 'input/TPT Digi Numbers Reported - Year3 AugOct21.csv'
 
 if __name__ == "__main__":
-    df = import_data(mypath)
+    df = cf.import_data(mypath)
