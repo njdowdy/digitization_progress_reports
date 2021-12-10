ae550c0e87d6acb8ca716210a6a8ac9ce07844ed
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-10 15:45:19 -0600

    Updated script to implement new functions

--- script.py
+++ script.py
@@ -1,10 +1,14 @@
-from functions import cleaning_funcs as cf
+from functions import cleaning_funcs as cle
+from functions import calculation_funcs as cal
+from functions import plotting_funcs as plt
 
 mypath = 'input/TPT Digi Numbers Reported - Year3 AugOct21.csv'
 
 if __name__ == "__main__":
-    df = cf.import_data(mypath)
-    df = cf.clean_import(df)
-    df = cf.make_totals_row(df)
-    df = cf.wide_to_long(df)
-
+    df = cle.import_data(mypath)
+    df = cle.clean_import(df)
+    df = cle.make_totals_row(df)
+    df = cle.wide_to_long(df)
+    df = cal.create_percentages(df)
+    df = cal.calculate_statistics(df)
+    plt.create_plots(df)
