865abc7b45195fe81dac94a40d3f57b2056b7d8f
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-14 16:06:28 -0600

    Reformatted to package-style

--- script.py
+++ script.py
@@ -1,19 +1,17 @@
-from functions import cleaning_funcs as cle
-from functions import calculation_funcs as cal
-from functions import plotting_funcs as plt
+from digitization_progress_reports.functions import cleaning_funcs as cle
+from digitization_progress_reports.functions import calculation_funcs as cal
+from digitization_progress_reports.functions import plotting_funcs as plt
 
-template_path = "input/input_template.csv"
-tpt = "input/TPT Digi Numbers Reported - Year3 AugOct21.csv"
+mypath = "digitization_progress_reports/input/input_template.csv"
+tpt = "digitization_progress_reports/input/TPT Digi Numbers Reported - Year3 AugOct21.csv"
+output = "digitization_progress_reports/output/figures/"
 
 if __name__ == "__main__":
-    # utilizing the generic template
-    df = cle.import_data(template_path)  # import data [see 'input/input_template.csv']
-    df = cle.clean_import_template(df)  # clean data [if following template]
-    # df = cle.clean_import_tpt(df)  # clean data [TPT-specific formatting]
-    df = cle.make_totals_row(
-        df, total_label="TOTAL", total_code="TOTAL"
-    )  # make totals row
+    df = cle.import_data(tpt)  # import data [see 'input/input_template.csv']
+    # df = cle.clean_import_template(df)  # clean data [if following template]
+    df = cle.clean_import_tpt(df)  # clean data [TPT-specific formatting]
+    df = cle.make_totals_row(df)  # make totals row
     df = cle.wide_to_long(df)  # convert to long data format
     df = cal.create_percentages(df)  # calculate percentages from reported values
     df = cal.calculate_statistics(df)  # calculate plotting statistics
-    plt.create_plots(df)  # plot statistics
+    plt.create_plots(df, output_path=output)  # plot statistics
