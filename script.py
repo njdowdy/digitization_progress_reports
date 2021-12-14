4ad57d2557d270d14b4af817e45adad1a5393498
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-14 15:12:02 -0600

    Various updates to critical functions to enhance output graphs, generalize to specific template file

--- script.py
+++ script.py
@@ -2,13 +2,18 @@ from functions import cleaning_funcs as cle
 from functions import calculation_funcs as cal
 from functions import plotting_funcs as plt
 
-mypath = 'input/TPT Digi Numbers Reported - Year3 AugOct21.csv'
+template_path = "input/input_template.csv"
+tpt = "input/TPT Digi Numbers Reported - Year3 AugOct21.csv"
 
 if __name__ == "__main__":
-    df = cle.import_data(mypath)
-    df = cle.clean_import(df)
-    df = cle.make_totals_row(df)
-    df = cle.wide_to_long(df)
-    df = cal.create_percentages(df)
-    df = cal.calculate_statistics(df)
-    plt.create_plots(df)
+    # utilizing the generic template
+    df = cle.import_data(template_path)  # import data [see 'input/input_template.csv']
+    df = cle.clean_import_template(df)  # clean data [if following template]
+    # df = cle.clean_import_tpt(df)  # clean data [TPT-specific formatting]
+    df = cle.make_totals_row(
+        df, total_label="TOTAL", total_code="TOTAL"
+    )  # make totals row
+    df = cle.wide_to_long(df)  # convert to long data format
+    df = cal.create_percentages(df)  # calculate percentages from reported values
+    df = cal.calculate_statistics(df)  # calculate plotting statistics
+    plt.create_plots(df)  # plot statistics
