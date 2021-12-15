8642dbc96b765763167b150a1fd30712576387b2
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-14 18:11:45 -0600

    Applied more sensible default settings for the general case

--- script.py
+++ script.py
@@ -3,13 +3,13 @@ from digitization_progress_reports.functions import calculation_funcs as cal
 from digitization_progress_reports.functions import plotting_funcs as plt
 
 mypath = "digitization_progress_reports/input/input_template.csv"
-tpt = "digitization_progress_reports/input/TPT Digi Numbers Reported - Year3 AugOct21.csv"
+# mypath = "digitization_progress_reports/input/TPT Digi Numbers Reported - Year3 AugOct21.csv"
 output = "digitization_progress_reports/output/figures/"
 
 if __name__ == "__main__":
-    df = cle.import_data(tpt)  # import data [see 'input/input_template.csv']
-    # df = cle.clean_import_template(df)  # clean data [if following template]
-    df = cle.clean_import_tpt(df)  # clean data [TPT-specific formatting]
+    df = cle.import_data(mypath)  # import data [see 'input/input_template.csv']
+    df = cle.clean_import_template(df)  # clean data [if following template]
+    # df = cle.clean_import_tpt(df)  # clean data [TPT-specific formatting]
     df = cle.make_totals_row(df)  # make totals row
     df = cle.wide_to_long(df)  # convert to long data format
     df = cal.create_percentages(df)  # calculate percentages from reported values
