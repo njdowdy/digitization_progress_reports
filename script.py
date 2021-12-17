8871e8c9a7f45e9614effc3385da23c3d10004b8
Nicolas J. Dowdy <njdowdy@gmail.com>
2021-12-17 07:57:03 -0600

    Implement def-main-name-main format to avoid scope shadowing

--- script.py
+++ script.py
@@ -2,10 +2,11 @@ import digitization_progress_reports as d
 
 mypath = "digitization_progress_reports/input/input_template.csv"
 # mypath = "digitization_progress_reports/input/TPT Digi Numbers Reported - Year3 AugOct21.csv"
-output = "digitization_progress_reports/output/figures/"
+myoutput = "digitization_progress_reports/output/figures/"
 
-if __name__ == "__main__":
-    df = d.clean.import_data(mypath)  # import data [see 'input/input_template.csv']
+
+def main(path: str, output: str) -> None:
+    df = d.clean.import_data(path)  # import data [see 'input/input_template.csv']
     df = d.clean.clean_import_template(df)  # clean data [if following template]
     # df = d.clean.clean_import_tpt(df)  # clean data [TPT-specific formatting]
     df = d.clean.make_totals_row(df)  # make totals row
@@ -13,3 +14,7 @@ if __name__ == "__main__":
     df = d.calc.create_percentages(df)  # calculate percentages from reported values
     df = d.calc.calculate_statistics(df)  # calculate plotting statistics
     d.plot.create_plots(df, output_path=output)  # plot statistics
+
+
+if __name__ == "__main__":
+    main(mypath, myoutput)
