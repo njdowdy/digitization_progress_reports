4964dc6e2e1c477862bb1936056ea9eb21137e09
Nicolas J. Dowdy <njdowdy@gmail.com>
2022-01-04 10:55:05 -0600

    Highlighted areas where code needs to be changed for TPT

--- digitization_progress_reports/run.py
+++ digitization_progress_reports/run.py
@@ -1,14 +1,17 @@
 import application as d
 
+# import os
+# os.chdir('./digitization_progress_reports')
+
 mypath = "application/input/input_template.csv"
-# mypath = "application/input/TPT Digi Numbers Reported - Year3 AugOct21.csv"
+#!TPT!# mypath = "application/input/TPT_YR3_NOV-JAN1.csv"
 myoutput = "application/output/figures/"
 
 
 def entrypoint(path: str, output: str) -> None:
     df = d.clean.import_data(path)  # import data [see 'input/input_template.csv']
     df = d.clean.clean_import_template(df)  # clean data [if following template]
-    # df = d.clean.clean_import_tpt(df)  # clean data [TPT-specific formatting]
+    #!TPT!# df = d.clean.clean_import_tpt(df)  # clean data [TPT-specific formatting]
     df = d.clean.make_totals_row(df)  # make totals row
     df = d.clean.wide_to_long(df)  # convert to long data format
     df = d.calc.create_percentages(df)  # calculate percentages from reported values
